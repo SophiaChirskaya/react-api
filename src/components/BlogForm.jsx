36ba6f11150e758526e0f29967c8cc13ba3b2ece
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-23 15:47:25 +0100

    CREATE final

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -82,7 +82,7 @@ import axios from "axios";
             // Chiamata API in POST inviando dati nuovo post
             axios.post("http://localhost:3000/posts", formData)
             .then(res => {
-                // Uso la risposta API per creare nuovo array dei posts
+            // Uso la risposta API per creare nuovo array dei posts
                 setPosts((currentPosts) => [...currentPosts, res.data])
             }
         )
